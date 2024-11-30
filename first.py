3500f28493c9589a1e0238a4ef3252aaf7466b97
pySpaceBremen <148010240+pyspacebremen@users.noreply.github.com>
2024-11-30 22:12:49 +0100

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,15 @@
+# pySpaceBremen
+# 2024-11-30
+# first.py
 
+from fasthtml import common as fh
+
+app, rt = fh.fast_app(live=True)
+
+
+@rt('/')
+def index():
+    return fh.P('Hallo Welt!')
+
+
+fh.serve()
